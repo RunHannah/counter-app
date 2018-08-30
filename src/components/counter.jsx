488c0d7a1dafadd4bde4d247c8d6e7c03b4ab5e5
Hannah Lee <hanlavlee@gmail.com>
2018-08-30 15:06:52 -0700

    Refactor Counter component and add increment and reset buttons to Counters component.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,7 @@
 import React, { Component } from "react";
 
+// refactored and removed local state, and rely on props from parent component
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-    // tags: ["tag1", "tag2", "tag3"]
-  };
-
-  //to bind event handlers, long way
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  // use this to bind event handler and change state
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     // console.log("props", this.props);
     return (
@@ -24,7 +9,7 @@ class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -41,12 +26,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
