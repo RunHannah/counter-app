c97e65d74712d6149629fc2e7a5cad88a4527734
Hannah Lee <hanlavlee@gmail.com>
2018-09-01 18:05:53 -0700

    Add increment button and disable decrement button if value is zero.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,15 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = counter => {
+    // cloning the array with spread operator
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -51,6 +60,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
